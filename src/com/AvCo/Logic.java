b46d433cdc762f8adb404164b24c7f6e516a534a
peterormai <ormaipeter.fx@gmail.com>
2017-08-15 17:46:18 +0200

    Create buildTable method in Logic class

--- src/com/AvCo/Logic.java
+++ src/com/AvCo/Logic.java
@@ -1,6 +1,30 @@
 package com.AvCo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Logic {
+
+    int tableSize;
+
+    public TicTacToe (int tableSize) {
+        this.tableSize = tableSize;
+    }
+
+    public List buildTable() {
+
+        List<List> table = new ArrayList<>();
+        List<String> rows = new ArrayList<>();
+
+        for(int j=0; j<tableSize; j++) {
+            rows.add("O");
+        }
+
+        for(int i=0; i<tableSize; i++) {
+            table.add(rows);
+        }
+
+        return table;
+    }
+
 }
-
-
