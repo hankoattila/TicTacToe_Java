d90bd258d62bb0eb0d3e26128e667996a8d71758
Gergely Németh <ngerle@gmail.com>
2017-08-16 10:52:30 +0200

    Add openFile() and readFile() to GameState class

--- src/com/AvCo/GameState.java
+++ src/com/AvCo/GameState.java
@@ -1,4 +1,37 @@
 package com.AvCo;
 
+import java.io.*;
+import java.net.URL;
+import java.util.*;
+
 public class GameState {
+
+    private Scanner inputFile = null;
+
+    void openFile(String filename, String operation) {
+        filename = "/files/" + filename;
+        URL url = getClass().getResource(filename);
+        filename = url.getFile();
+        try {
+            if (operation.equals("read")) {
+                inputFile = new Scanner(new BufferedReader(new FileReader(filename)));
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+    List<List<String>> readFile() {
+        List<List<String>> strList = new ArrayList<>();
+        while (inputFile.hasNext()) {
+            List<String> subList = new ArrayList<>();
+            String input = inputFile.nextLine();
+            String[] strArr = input.split(",");
+            for (String elem : strArr) {
+                subList.add(elem);
+            }
+            strList.add(subList);
+        }
+        return strList;
+    }
 }
