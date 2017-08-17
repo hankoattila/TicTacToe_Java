eb55d5034bab4eac2196db300b9ad199db6d4fec
Gergely Németh <ngerle@gmail.com>
2017-08-17 10:05:08 +0200

    Make openFile() in GameState to make append possible and some bugfix

--- src/com/AvCo/GameState.java
+++ src/com/AvCo/GameState.java
@@ -1,24 +1,35 @@
 package com.AvCo;
 
 import java.io.*;
-import java.lang.reflect.Array;
-import java.net.URL;
 import java.util.*;
 
 public class GameState {
 
     private Scanner inputFile = null;
     private BufferedWriter outputFile = null;
+    private int numOfSavedGames = 0;
 
     void openFile(String filename, String operation) {
-        filename = "/files/" + filename;
-        URL url = getClass().getResource(filename);
-        filename = url.getFile();
+        filename = "out/production/TicTacToe_Java/files/" + filename;
+
+
         try {
             if (operation.equals("read")) {
                 inputFile = new Scanner(new BufferedReader(new FileReader(filename)));
             } else if (operation.equals("write")) {
-                outputFile = new BufferedWriter(new FileWriter(filename));
+                // Get number of saves games
+                inputFile = new Scanner(new BufferedReader(new FileReader(filename)));
+                numOfSavedGames = readFile().size();
+                closeFile(inputFile);
+
+                // New input for writing
+                inputFile = new Scanner(new BufferedReader(new FileReader(filename)));
+                if (numOfSavedGames != 0) {
+                    outputFile = new BufferedWriter(new FileWriter(filename, true));
+                } else {
+                    outputFile = new BufferedWriter(new FileWriter(filename));
+                }
+                closeFile(inputFile);
             }
         } catch (Exception e) {
             e.getStackTrace();
@@ -33,16 +44,16 @@ public class GameState {
         return outputFile;
     }
 
-    List<HashMap<String,String>> readFile() {
-        List<HashMap<String,String>> mapList = new ArrayList<>();
+    List<HashMap<String, String>> readFile() {
+        List<HashMap<String, String>> mapList = new ArrayList<>();
         while (inputFile.hasNext()) {
             String[] subList;
             String input = inputFile.nextLine();
             subList = input.split(":");
-            String[] keyArray = {"id","table","player1","player2","nextPlayer"};
-            HashMap<String,String> addNewMap = new HashMap<>();
-            for (int i = 0; i< subList.length; i++){
-                addNewMap.put(keyArray[i],subList[i]);
+            String[] keyArray = {"id", "table", "player1", "player2", "nextPlayer"};
+            HashMap<String, String> addNewMap = new HashMap<>();
+            for (int i = 0; i < subList.length; i++) {
+                addNewMap.put(keyArray[i], subList[i]);
             }
             mapList.add(addNewMap);
 
@@ -50,28 +61,30 @@ public class GameState {
 
         return mapList;
     }
-    public List<String> oneDimesionTable(String stringTable){
+
+    public List<String> oneDimesionTable(String stringTable) {
         List<String> oneDimensionList = new ArrayList<>();
-        String[] splitedTable = stringTable.split("@");
-        for(String stringRow: splitedTable){
-            String[] splitedRow = stringRow.split(",");
-            for (String field: splitedRow ){
+        String[] splitTable = stringTable.split("@");
+        for (String stringRow : splitTable) {
+            String[] splitRow = stringRow.split(",");
+            for (String field : splitRow) {
                 oneDimensionList.add(field);
             }
 
         }
         return oneDimensionList;
     }
-    public List<List<String>> twoDimensionTable(String stringTable){
+
+    public List<List<String>> twoDimensionTable(String stringTable) {
         List<List<String>> twoDimensionList = new ArrayList<>();
-        String[] splitedArray = stringTable.split("@");
-        for (int i = 0; i< splitedArray.length;i++){
-                String[] newArray = splitedArray[i].split(",");
-                List<String> addNew = new ArrayList<>();
-                for (int j = 0; j<newArray.length;j++){
-                    addNew.add(newArray[j]);
-                }
-                twoDimensionList.add(addNew);
+        String[] splitArray = stringTable.split("@");
+        for (int i = 0; i < splitArray.length; i++) {
+            String[] newArray = splitArray[i].split(",");
+            List<String> addNew = new ArrayList<>();
+            for (int j = 0; j < newArray.length; j++) {
+                addNew.add(newArray[j]);
+            }
+            twoDimensionList.add(addNew);
 
         }
         return twoDimensionList;
@@ -84,16 +97,16 @@ public class GameState {
         }
     }
 
-    void writeFile(List<List<String>> table,String player,String player2, String nextPlayer) {
-        String writeString = "0:";
+    void writeFile(List<List<String>> table, String player, String player2, int nextPlayer) {
+        String writeString = Integer.toString(numOfSavedGames) + ":";
+        List<String> tempGameStateList = new ArrayList<>();
+        for (List<String> row : table) {
+            tempGameStateList.add(String.join(",", row));
+
+        }
+        writeString += String.join("@", tempGameStateList);
+        writeString += ':' + player + ':' + player2 + ':' + nextPlayer + "\n";
         try {
-
-            for (List<String> row : table) {
-                writeString += String.join("$", row);
-                writeString += "@";
-            }
-            writeString += ':' + player + ':' + player2 + ':' + nextPlayer;
-
             outputFile.write(writeString);
         } catch (IOException e) {
             System.out.println("IOException: " + e);
